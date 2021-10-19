7cb6a99c506a666caa19f8a9615d637d0e698df0
Chris Penn <chrispenn26@gmail.com>
2021-10-19 21:19:33 +0800

    Upload from laptop

---
+++ Resources/Code/QuickDBD-ETL database schema.sql
@@ -0,0 +1,33 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/2hvokS
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "postcode_vehicle_statistics" (
+    "Postcode" int   NOT NULL,
+    "State_abrev" varchar   NOT NULL,
+    "Avg_price" Double   NOT NULL,
+    "Med_price" Double   NOT NULL,
+    "Avg_age" Double   NOT NULL,
+    "Med_age" Double   NOT NULL,
+    "No_listings" Double   NOT NULL,
+    "Date_updated" Date   NOT NULL,
+    CONSTRAINT "pk_postcode_vehicle_statistics" PRIMARY KEY (
+        "Postcode"
+     )
+);
+
+CREATE TABLE "ATO_taxable_income" (
+    "State_abrev" varchar   NOT NULL,
+    "Postcode" int   NOT NULL,
+    "Num_individuals" int   NOT NULL,
+    "Taxable_income" int   NOT NULL,
+    "Total_work_related_car_exp" int   NOT NULL,
+    CONSTRAINT "pk_ATO_taxable_income" PRIMARY KEY (
+        "Postcode"
+     )
+);
+
---
+++ Resources/app/Website/app.js
@@ -0,0 +1,179 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "Avg_taxable_income";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(listingData, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(listingData, d => d[chosenXAxis]) * 0.8,
+      d3.max(listingData, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+
+}
+
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  return xAxis;
+}
+
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+
+  return circlesGroup;
+}
+
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, circlesGroup) {
+
+  var label;
+
+  if (chosenXAxis === "Avg_taxable_income") {
+    label = "Average Taxable Income: ";
+  }
+  else {
+    label = "Avg. Work Related Car Expenses: ";
+  }
+
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+      return (`${d.Suburb}<br>${label} ${d[chosenXAxis]}`);
+    });
+
+  circlesGroup.call(toolTip);
+
+  circlesGroup.on("mouseover", function(data) {
+    toolTip.show(data);
+  })
+    // onmouseout event
+    .on("mouseout", function(data, index) {
+      toolTip.hide(data);
+    });
+
+  return circlesGroup;
+}
+
+
+// Import Data
+d3.json("../../Data/Final_combined_data/listing_vs_abs_data.json").then(function(listingData) {
+
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    listingData.forEach(function(data) {
+      data.Avg_Income = +data.Avg_Income;
+      data.Average_Price = +data.Average_Price;
+      data.Avg_WR_car_expense;
+    });
+
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([20000, d3.max(listingData, d => d.Avg_Income)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([-10000, d3.max(listingData, d => d.Average_Price)])
+      .range([height, 0]);
+
+    // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(listingData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.Avg_Income))
+    .attr("cy", d => yLinearScale(d.Average_Price))
+    .attr("r", "12")
+    .attr("fill", "blue")
+    .attr("opacity", ".3");
+
+    // Step 6: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([10, -10])
+      .html(function(d) {
+        return (`${d.Postcode}, ${d.State}<br>Average taxable income: ${d.Avg_Income}<br>Average vehicle listing price: ${d.Average_Price}<br>Number of cars listed for sale: ${d.Count_of_listings}`);
+      });
+
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("hover", function(data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 30)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Average Price of Cars Listed for Sale ($)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Average Taxable Income ($)");
+  }).catch(function(error) {
+    console.log(error);
+  });
